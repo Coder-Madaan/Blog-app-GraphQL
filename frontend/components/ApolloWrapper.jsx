4c9cb77c0b639c958241d61c764710257bb453a1
Vaibhav <madaanvaibhav001@gmail.com>
2025-05-20 14:31:27 +0530

    removed pinging

--- frontend/components/ApolloWrapper.jsx
+++ frontend/components/ApolloWrapper.jsx
@@ -4,7 +4,7 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 export function ApolloWrapper({ children }) {
   const client = new ApolloClient({
-    uri: `${process.env.NEXT_PUBLIC_API_URL}/graphql`,
+    uri: `${process.env.NEXT_PUBLIC_API_URL}`,
     cache: new InMemoryCache(),
   });
 
